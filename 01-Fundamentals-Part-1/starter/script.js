1b5b1e6fccec6019015430700d114c1f924a95c2
Keiran Brown <keiranbrown1301@gmail.com>
2023-02-07 20:41:03 +0900

    learned about values and variables.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,27 @@
 let js = "amazing";
-if (js === "amazing") alert("Javascript is FUN!");
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("Keiran");
+console.log(32);
+
+let firstName = "Megumi";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable is a box which we give a name!
+
+// let 3years = 3
+// Variable names cannot start with a number
+
+// let keiran&meg = 'KM'
+// cannot use the ampersand
+
+// Various variable names are a illegal - number, function
+
+// Do not start with an uppercase name, as it's used for OOP
+
+// Variable names should be descriptive.
+// let myFirstJob = 'teacher' <- YES!
+// let job1 = 'teacher' <- NO!
