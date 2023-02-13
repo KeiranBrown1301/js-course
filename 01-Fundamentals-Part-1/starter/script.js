912257620c911299c64fecff637ff499a9ab1ab4
Keiran Brown <keiranbrown1301@gmail.com>
2023-02-13 12:46:23 +0900

    challenge  completed 3

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -190,8 +190,147 @@
 // }
 
 // TYPE CONVERSION
-const inputYear = "1991";
-console.log(Number(inputYear));
-console.log(inputYear + 18);
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number("Keiran"));
+// console.log(typeof NaN);
+
+// console.log(String(32), 32);
 
 // TYPE COESCION
+// console.log("I am " + 32 + " years old.");
+// console.log("23" - "10" - 3);
+// console.log("23" * "2");
+
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
+
+// TRUTHY FALSY
+//  FALSY - 0, '', undefined, null, NaN
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Keiran"));
+// console.log(Boolean(""));
+// console.log(Boolean({}));
+
+// const money = 10;
+// if (money) {
+//   console.log("Dont spent it all!");
+// } else {
+//   console.log("You should get a job.");
+// }
+
+// let height = 180;
+// if (height) {
+//   console.log("Yay height is defined");
+// } else {
+//   console.log("Geight is UNDEFINED");
+// }
+
+// Equality operators
+// const age = 18;
+// if (age === 18) console.log("You just became an adult! (strict)");
+// if (age == 18) console.log("You just became an adult! (loose)");
+//  === is a strict equality operator - No type coercion
+//  == loose operator - Returns type coercion. NOTE: Generally avoided.
+// Default the triple operator.
+
+// const favourite = Number(prompt("What's your favourite number?"));
+// console.log(favourite);
+// console.log(typeof favourite);
+
+// if (favourite === 23) {
+//   console.log("Cool! 23 is mine too!");
+// } else if (favourite === 7) {
+//   console.log("7 is also a pretty cool number");
+// } else {
+//   console.log("That isnt 23 or 7... Damn");
+// }
+
+// if (favourite !== 23) console.log("Why not 23?");
+//  !== - Strict
+//  != - Loose
+
+// Boolean Operators
+// And, Or or Not
+//  Not operator = !A
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision); // AND
+console.log(hasDriversLicense || hasGoodVision); // OR
+console.log(!hasDriversLicense); // NOT
+
+// const shouldDrive = hasDriversLicense && hasGoodVision;
+
+// if (shouldDrive) {
+//   console.log(`Sarash is able to drive`);
+// } else {
+//   console.log(`Someone else can drive.`);
+// }
+
+// const isTired = false; // C
+// console.log(hasDriversLicense && hasGoodVision && !isTired); // OR
+
+// if (hasDriversLicense && hasGoodVision && !isTired) {
+//   console.log(`Sarash is able to drive.`);
+// } else {
+//   console.log(`Someone else should drive...`);
+// }
+
+//  CODING CHALLENGE 3
+// Dolphins win
+// const dolphinsAverageScore = (96 + 108 + 89) / 3;
+// const koalasAverageScore = (88 + 91 + 110) / 3;
+
+// Koalas win
+// const dolphinsAverageScore = (96 + 108 + 89) / 3;
+// const koalasAverageScore = (99 + 150 + 110) / 3;
+
+// Draw
+// const dolphinsAverageScore = (96 + 108 + 89) / 3;
+// const koalasAverageScore = (96 + 108 + 89) / 3;
+
+// console.log(dolphinsAverageScore);
+// console.log(koalasAverageScore);
+
+// if (dolphinsAverageScore > koalasAverageScore) {
+//   console.log("The Dolphins are the winners!");
+// } else if (dolphinsAverageScore === koalasAverageScore) {
+//   console.log(`It's a draw!`);
+// } else {
+//   console.log(`The Koalas are the winners!`);
+// }
+
+// Bonus 1
+// const dolphinsAverageScore = (97 + 112 + 101) / 3;
+// const koalasAverageScore = (109 + 95 + 123) / 3;
+
+//  Bonus 2
+const dolphinsAverageScore = (97 + 112 + 101) / 3;
+const koalasAverageScore = (109 + 95 + 106) / 3;
+
+console.log(dolphinsAverageScore);
+console.log(koalasAverageScore);
+
+const minimumScore = 100;
+
+if (
+  dolphinsAverageScore > koalasAverageScore &&
+  dolphinsAverageScore >= minimumScore
+) {
+  console.log(`The Dolphins are the winners!`);
+} else if (
+  koalasAverageScore > dolphinsAverageScore &&
+  koalasAverageScore >= minimumScore
+) {
+  console.log(`The Koalas are the winners!`);
+} else if (
+  (koalasAverageScore < minimumScore && dolphinsAverageScore < minimumScore) ||
+  koalasAverageScore === dolphinsAverageScore
+) {
+  console.log(`It's a draw`);
+}
