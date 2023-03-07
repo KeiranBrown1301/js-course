48df104bc3f070e79390680c4427c3b9a592e3bf
Keiran Brown <keiranbrown1301@gmail.com>
2023-03-07 22:21:30 +0900

    updated workings and also completed bonuc calcAverage function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -458,10 +458,31 @@ const calcTip = function (bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 };
 
+// My workings
 for (let i = 0; i < bills.length; i++) {
   tips.push(calcTip(bills[i]));
   totals.push(bills[i] + tips[i]);
 }
 
+// His workings
+// for (let i = 0; i < bills.length; i++) {
+//   const tip = calcTip(bills[i]);
+//   tips.push(tip);
+//   totals.push(tip + bills[i]);
+// }
+
 console.log(tips);
 console.log(totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(tips));
+console.log(calcAverage(totals));
