da2d708f041533715aae3599e2c9e4399c3323de
Keiran Brown <keiranbrown1301@gmail.com>
2023-03-10 08:45:14 +0900

    notes taken for 'problem solving' ideas.

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -5,3 +5,16 @@ const x = 23;
 
 const calcAge = birthYear => 2023 - birthYear;
 console.log(calcAge(1990));
+
+// Solving problems as a developer.
+
+// Example - In an array of GPS coordinates, find the two closest points.
+// Stay calm and slow down.
+// Take a logical and rational approach.
+
+// 4 steps.
+// 1 - Make sure you 100% understand the problem. Ask the right questions to get a clear picture of the problem.
+// 2 - Divide and conquer: Break a big problem into smaller sub problems.
+// 3 - Dont be afraid to do as much research as we have to. (Google, Stack overflow, MDN.)
+// 4 - For bigger problems, write pusedo code before writing the actual code.
+//
