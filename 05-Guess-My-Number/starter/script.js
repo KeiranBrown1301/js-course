abc94b6ff6020560ea25d53c0f490942d631d42c
Keiran Brown <keiranbrown1301@gmail.com>
2023-03-10 12:26:57 +0900

    notes on DOM manipulation added.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,11 @@
 'use strict';
 
+// What the DOM actually be?
+// Document object model.
+// Structured representation of HTML documents. Allows JS to access HTML elements and styles to manipulate them. Simply put: Change text, HTML attributes and even CSS styles.
+
+// Special object that is the entry point to the DOM is DOCUMENT -> document.querySelector()
+
+// DOM methods and properties for DOM manipulation IS NOT part of JS. It's part of the WEB apis and CAN INTERACT with JS. More of these web apis, can be for example FETCH or TIMERS, etc.
+
 console.log(document.querySelector('.message').textContent);
