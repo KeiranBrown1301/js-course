65fd85faa6d425c8cb2fb48922333c17c4c4f068
Keiran Brown <keiranbrown1301@gmail.com>
2023-03-16 09:57:24 +0900

    onclick added.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -8,13 +8,22 @@
 
 // DOM methods and properties for DOM manipulation IS NOT part of JS. It's part of the WEB apis and CAN INTERACT with JS. More of these web apis, can be for example FETCH or TIMERS, etc.
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'Correct Number! 🎉';
-console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number! 🎉';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 25;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 25;
 
-document.querySelector('.guess').value = 0;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 0;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number ⛔️';
+  }
+});
