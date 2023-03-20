e5c9ac4c8e69201a5635b2a9e51cd381e3576073
Keiran Brown <keiranbrown1301@gmail.com>
2023-03-20 09:30:52 +0900

    added highscore logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -21,6 +21,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -36,6 +37,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+    }
+
+    document.querySelector('.highscore').textContent = highScore;
 
     // When player guesses too high.
   } else if (guess > secretNumber) {
