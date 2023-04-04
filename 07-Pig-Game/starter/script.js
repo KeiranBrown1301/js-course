3788cb946a2d0f28bd64b8eaa1985ec53b6c4d12
Keiran Brown <keiranbrown1301@gmail.com>
2023-04-04 20:46:44 +0900

    small fix that was wiping both scores on 1 roll.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -15,7 +15,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -30,8 +32,12 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add to current score.
     currentScore += dice;
-    current0El.textContent = currentScore; // Change Later!
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    // Switch to net player.
+    // Switch to next player.
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
   }
 });
