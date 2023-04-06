37efa9bb2f6fe74225dd2865c3bb91dd576144dd
Keiran Brown <keiranbrown1301@gmail.com>
2023-04-06 23:54:22 +0900

    hoisting and TDZ hands on practice.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -36,3 +36,47 @@ const firstName = 'Keiran';
 calcAge(1991);
 // console.log(age);
 // printAge()
+
+//  Hoisting and TDZ practice
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Keiran';
+let job = 'Teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
