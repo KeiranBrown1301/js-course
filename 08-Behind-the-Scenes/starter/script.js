4995869bd1df5cd4da6b9621cf37d25a7876fac2
Keiran Brown <keiranbrown1301@gmail.com>
2023-04-07 08:54:51 +0900

    this keyword practical use

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,39 +1,39 @@
 'use strict';
 
-function calcAge2(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge2(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      // Creating new variable with same name as outher scopes variable.
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       // Creating new variable with same name as outher scopes variable.
 
-      // Reasigning outer scopes variable
-      output = 'NEW OUTPUT!';
+//       // Reasigning outer scopes variable
+//       output = 'NEW OUTPUT!';
 
-      const firstName = 'John';
-      const str = `Oh, and youre a millenial, ${firstName}.`;
-      console.log(str);
+//       const firstName = 'John';
+//       const str = `Oh, and youre a millenial, ${firstName}.`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
-    // console.log(str); // Block scoped;
-    console.log(millenial); // Function scoped;
-    // console.log(add(2, 3));
-    console.log(output);
-  }
-  printAge();
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     // console.log(str); // Block scoped;
+//     console.log(millenial); // Function scoped;
+//     // console.log(add(2, 3));
+//     console.log(output);
+//   }
+//   printAge();
 
-  return age;
-}
+//   return age;
+// }
 
-const firstName = 'Keiran';
-calcAge2(1991);
+// const firstName = 'Keiran';
+// calcAge2(1991);
 // console.log(age);
 // printAge()
 
@@ -57,11 +57,11 @@ function addDecl(a, b) {
   return a + b;
 }
 
-const addExpr = function (a, b) {
-  return a + b;
-};
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
 
-var addArrow = (a, b) => a + b;
+// var addArrow = (a, b) => a + b;
 
 // Example
 console.log(undefined);
@@ -83,33 +83,81 @@ console.log(z === window.z);
 
 // console.log(this);
 
-const calcAge = function (birthYear) {
-  console.log(2037 - birthYear);
-  // console.log(this);
-};
-calcAge(1991);
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   // console.log(this);
+// };
+// calcAge(1991);
 
-const calcAgeArrow = birthYear => {
-  console.log(2037 - birthYear);
-  // console.log(this);
-};
-calcAgeArrow(1991);
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   // console.log(this);
+// };
+// calcAgeArrow(1991);
+
+// const keiran = {
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   },
+// };
+// keiran.calcAge();
+
+// const megumi = {
+//   year: 1995,
+// };
+
+// megumi.calcAge = keiran.calcAge;
+// megumi.calcAge();
+
+// const f = keiran.calcAge;
+// f();
+
+// var firstName = 'Megumi';
 
 const keiran = {
+  firstName: 'Keiran',
   year: 1991,
   calcAge: function () {
-    console.log(this);
+    // console.log(this);
     console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1991);
+    //   // console.log(this.year >= 1981 && this.year <= 1991);
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1991);
+    };
+    isMillenial();
   },
+  // Arrow functions do not get a this keyword
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+  // greet: () => console.log(`Hey ${keiran.firstName}`),
 };
+keiran.greet();
 keiran.calcAge();
 
-const megumi = {
-  year: 1995,
+// Arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
 };
+addExpr(2, 5);
+addExpr(2, 5, 8, 16);
 
-megumi.calcAge = keiran.calcAge;
-megumi.calcAge();
-
-const f = keiran.calcAge;
-f();
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
