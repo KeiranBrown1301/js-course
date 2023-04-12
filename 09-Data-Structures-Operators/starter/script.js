7cecba430d49ee87722505a186f823bff62eac70
Keiran Brown <keiranbrown1301@gmail.com>
2023-04-12 18:39:57 +0900

    rest operator and short circuiting

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -45,59 +45,134 @@ const restaurant = {
   orderPasta: function (ing1, ing2, ing3) {
     console.log(`Here is your pasta with with ${ing1}, ${ing2} and ${ing3}`);
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
-const arr = [7, 8, 9];
-const badNewArray = [1, 2, arr[0], arr[1], arr[2]];
-console.log(badNewArray);
+// SHORT CIRCUITING
+// OR OPERATOR = Great way to set default values
+// Returns the first truthy or last element
+// can use any data type, return any data type, short circuiting
+console.log('--------OR-----------');
+console.log(2 || 'Keiran');
+console.log('' || 'Keiran');
+console.log(true || 0);
+console.log(undefined || null);
 
-const newArr = [1, 2, ...arr];
-console.log(newArr);
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
 
-console.log(...newArr);
-console.log(1, 2, 7, 8, 9);
+// restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
 
-const newMenu = [...restaurant.mainMenu, 'Gnocchi'];
-console.log(newMenu);
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
 
-// Copy array
-const mainMenuCopy = [...restaurant.mainMenu];
+console.log('-----------AND------------');
+console.log(0 && 'Keiran');
+console.log(7 && 'Keiran');
+console.log('Hello' && 23 && null && 'Keiran');
 
-// Join 2 arrays or more
-const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
-console.log(menu);
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'pineapple');
+}
 
-// Iterables: Arrays, strings, maps, sets. NOT objects.
-const str = 'Keiran';
-const letters = [...str, ' ', 'B.'];
-console.log(letters);
-console.log(...str);
-console.log('K', 'e');
-// console.log(`${...str} Brown`);
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+// AND OPERATOR
 
-// Real world example
-const ingredients = [
-  prompt("Let's make pasta! Ingredient 1?"),
-  prompt('Ingredient 2?'),
-  prompt('Ingredient 3?'),
-];
-console.log(ingredients);
+// // REST PATTERN
+// // Destructuring
+// // Spread because on the right side of the =
+// const arr = [1, 2, ...[3, 4]];
+// // REST because on the LEFT of the =
+// const [a, b, ...other] = [1, 2, 3, 4, 5];
+// console.log(a, b, other);
+// // REST always has to be the last element. Can only be 1 REST in any assignment.
+// const [pizza, , risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+// console.log(pizza, risotto, otherFood);
 
-// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
-restaurant.orderPasta(...ingredients);
+// // Objects
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
 
-// Objects
-const newRestaraunt = {
-  foundingYear: 1991,
-  ...restaurant,
-  founder: 'Keiran Brown',
-};
-console.log(newRestaraunt);
+// // Functions
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+//   console.log(sum);
+// };
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(8, 2, 3, 4, 5, 6, 9);
 
-const restaurantCopy = { ...restaurant };
-restaurantCopy.name = 'Ristorante Roma';
-console.log(restaurantCopy.name);
-console.log(restaurant.name);
+// const x = [23, 5, 7];
+// add(...x);
+
+// restaurant.orderPizza('mushroom', 'pineapple', 'onions');
+// restaurant.orderPizza('Mushrooms');
+// END OF REST PATTERN
+
+// SPREAD OPERATOR
+// const arr = [7, 8, 9];
+// const badNewArray = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArray);
+
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
+
+// console.log(...newArr);
+// console.log(1, 2, 7, 8, 9);
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocchi'];
+// console.log(newMenu);
+
+// // Copy array
+// const mainMenuCopy = [...restaurant.mainMenu];
+
+// // Join 2 arrays or more
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(menu);
+
+// // Iterables: Arrays, strings, maps, sets. NOT objects.
+// const str = 'Keiran';
+// const letters = [...str, ' ', 'B.'];
+// console.log(letters);
+// console.log(...str);
+// console.log('K', 'e');
+// // console.log(`${...str} Brown`);
+
+// // Real world example
+// const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt('Ingredient 2?'),
+//   prompt('Ingredient 3?'),
+// ];
+// console.log(ingredients);
+
+// // restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// restaurant.orderPasta(...ingredients);
+
+// // Objects
+// const newRestaraunt = {
+//   foundingYear: 1991,
+//   ...restaurant,
+//   founder: 'Keiran Brown',
+// };
+// console.log(newRestaraunt);
+
+// const restaurantCopy = { ...restaurant };
+// restaurantCopy.name = 'Ristorante Roma';
+// console.log(restaurantCopy.name);
+// console.log(restaurant.name);
+// END OF SPREAD OPERATOR
+
 // DESTRUCTURING OBJECTS
 // restaurant.orderDelivery({
 //   time: '22.30',
