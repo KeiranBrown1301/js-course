f1e2d1dda17b7276bdb0f6573cb49e928c248355
Keiran Brown <keiranbrown1301@gmail.com>
2023-04-12 20:24:01 +0900

    coding challenge completed.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -70,17 +70,17 @@ const restaurant2 = {
 // restaurant2.numGuests ||= 10;
 
 // Nullish assignment operator (null or undefined)
-restaurant1.numGuests ??= 10;
-restaurant2.numGuests ??= 10;
+// restaurant1.numGuests ??= 10;
+// restaurant2.numGuests ??= 10;
 
 // restaurant1.owner = restaurant1.owner && '<ANONYMOUS>';
 // restaurant2.owner = restaurant2.owner && '<ANONYMOUS>';
 
-restaurant1.owner &&= '< ANONYMOUS >';
-restaurant2.owner &&= '< ANONYMOUS >';
+// restaurant1.owner &&= '< ANONYMOUS >';
+// restaurant2.owner &&= '< ANONYMOUS >';
 
-console.log(restaurant1);
-console.log(restaurant2);
+// console.log(restaurant1);
+// console.log(restaurant2);
 
 // NULLISH coalescing Operator
 // restaurant.numGuests = 0;
@@ -293,3 +293,77 @@ console.log(restaurant2);
 // console.log(p, q, r);
 
 // END OF DESTRUCURING ARRAYS ///
+
+// CODNG CHALLENGE!!
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1
+const [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
+// 2
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+// 3
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+// 4
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+// 5
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+// 6
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored`);
+  console.log(`The players that scored were ${players}`);
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals('Davies', 'Muller');
+printGoals(...game.scored);
+
+// 7
+team1 < team2 && console.log('Team 1 is more likely to win!');
+team2 < team1 && console.log('Team 2 is more likely to win!');
